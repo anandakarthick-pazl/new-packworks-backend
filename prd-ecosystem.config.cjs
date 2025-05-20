a9a0d664b23812033ea3172957a2f2db5abf8d0b
anandakarthick <anandakarthick.s1994@gmail.com>
2025-05-20 14:20:27 +0530

    code pushed
    code pushed

--- prd-ecosystem.config.cjs
+++ prd-ecosystem.config.cjs
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   apps: [
     {
@@ -5,7 +7,7 @@ module.exports = {
       script: "./services/company/server.js",
       watch: false,
       env: {
-        PORT: 3001,
+        PORT: process.env.PORT_COMPANY,
       },
     },
     {
@@ -13,7 +15,7 @@ module.exports = {
       script: "./services/user-service/server.js",
       watch: false,
       env: {
-        PORT: 3002,
+        PORT: process.env.PORT_USER,
       },
     },
     {
@@ -21,7 +23,7 @@ module.exports = {
       script: "./services/Client/server.js",
       watch: false,
       env: {
-        PORT: 3003,
+        PORT: process.env.PORT_CLIENT,
       },
     },
     {
@@ -29,7 +31,7 @@ module.exports = {
       script: "./services/sku/server.js",
       watch: false,
       env: {
-        PORT: 3004,
+        PORT: process.env.PORT_SKU,
       },
     },
     {
@@ -37,7 +39,7 @@ module.exports = {
       script: "./services/salesOrder/server.js",
       watch: false,
       env: {
-        PORT: 3005,
+        PORT: process.env.PORT_SALES_ORDER,
       },
     },
     {
@@ -45,181 +47,175 @@ module.exports = {
       script: "./services/workOrder/server.js",
       watch: false,
       env: {
-        PORT: 3006,
+        PORT: process.env.PORT_WORK_ORDER,
       },
     },
     {
       name: "machine-Service",
       script: "./services/machine/server.js",
       env: {
-        PORT: 3007,
+        PORT: process.env.PORT_MACHINE,
       },
     },
     {
       name: "common-Service",
       script: "./services/common-service/server.js",
       env: {
-        PORT: 3008,
+        PORT: process.env.PORT_COMMON,
       },
     },
     {
       name: "rbac-Service",
       script: "./services/rbac/server.js",
       env: {
-        PORT: 3009,
+        PORT: process.env.PORT_RBAC,
       },
     },
     {
       name: "department-Service",
       script: "./services/department/server.js",
       env: {
-        PORT: 3010,
+        PORT: process.env.PORT_DEPARTMENT,
       },
     },
     {
       name: "designation-Service",
       script: "./services/designations/server.js",
       env: {
-        PORT: 3011,
+        PORT: process.env.PORT_DESIGNATION,
       },
     },
     {
       name: "role-Service",
       script: "./services/role/server.js",
       env: {
-        PORT: 3012,
+        PORT: process.env.PORT_ROLE,
       },
     },
     {
       name: "storage-Service",
       script: "./services/storage/server.js",
       env: {
-        PORT: 3013,
+        PORT: process.env.PORT_STORAGE,
       },
     },
     {
       name: "company address-Service",
       script: "./services/company-address/server.js",
       env: {
-        PORT: 3014,
+        PORT: process.env.PORT_COMPANY_ADDRESS,
       },
     },
     {
       name: "File View-Service",
       script: "./services/upload-view/server.js",
       env: {
-        PORT: 3015,
+        PORT: process.env.PORT_FILE_VIEW,
       },
     },
     {
       name: "package",
       script: "./services/package/server.js",
-
       env: {
-        PORT: 3016,
+        PORT: process.env.PORT_PACKAGE,
       },
     },
     {
       name: "billing-Service",
       script: "./services/billing/server.js",
-
       env: {
-        PORT: 3017,
+        PORT: process.env.PORT_BILLING,
       },
     },
     {
       name: "faqs-Service",
       script: "./services/faqs/server.js",
-
       env: {
-        PORT: 3018,
+        PORT: process.env.PORT_FAQS,
       },
     },
     {
       name: "faq_categories-Service",
       script: "./services/faq-categories/server.js",
-
       env: {
-        PORT: 3019,
+        PORT: process.env.PORT_FAQ_CATEGORIES,
       },
     },
     {
       name: "swagger",
       script: "./config/swagger.js",
       env: {
-        PORT: 3020,
+        PORT: process.env.PORT_SWAGGER,
       },
     },
     {
       name: "taxes",
       script: "./services/taxes/server.js",
       env: {
-        PORT: 3021,
+        PORT: process.env.PORT_TAXES,
       },
     },
     {
       name: "item",
       script: "./services/item/server.js",
       env: {
-        PORT: 3022,
+        PORT: process.env.PORT_ITEM,
       },
     },
-
     {
       name: "purchase",
       script: "./services/purchase-order/server.js",
       env: {
-        PORT: 3023,
+        PORT: process.env.PORT_PURCHASE,
       },
     },
     {
       name: "grn",
       script: "./services/grn/server.js",
       env: {
-        PORT: 3024,
+        PORT: process.env.PORT_GRN,
       },
     },
     {
       name: "inventory",
       script: "./services/inventory/server.js",
       env: {
-        PORT: 3025,
+        PORT: process.env.PORT_INVENTORY,
       },
     },
-
     {
       name: "attendance-service",
       script: "./services/attendance/server.js",
       env: {
-        PORT: 3026,
+        PORT: process.env.PORT_ATTENDANCE,
       },
     },
     {
       name: "route",
       script: "./services/route/server.js",
       env: {
-        PORT: 3027,
+        PORT: process.env.PORT_ROUTE,
       },
     },
     {
       name: "purchase-return",
       script: "./services/purchase-order-return/server.js",
       env: {
-        PORT: 3028,
+        PORT: process.env.PORT_PURCHASE_RETURN,
       },
     },
     {
       name: "production",
       script: "./services/Production/server.js",
       env: {
-        PORT: 3029,
+        PORT: process.env.PORT_PRODUCTION,
       },
     },
     {
       name: "work-invoice",
       script: "./services/work-invoice/server.js",
       env: {
-        PORT: 3030,
+        PORT: process.env.PORT_WORK_INVOICE,
       },
     },
   ],
