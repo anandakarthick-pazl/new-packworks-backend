1d6f16ecff3911a2f21e433ea6582ba3f1c62d17
Columbus-12 <columbus.d@pazl.info>
2025-06-05 15:27:18 +0530

    migrate

--- migrations/20250604181004-add-machine.cjs
+++ migrations/20250604181004-add-machine.cjs
@@ -3,16 +3,16 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
-      ALTER TABLE machines
+      ALTER TABLE work_order
       ADD COLUMN unit VARCHAR(255) NULL,
-      ADD COLUMN board_length INT NULL,
-      ADD COLUMN board_width INT NULL;
+      ADD COLUMN board_length FLOAT NULL,
+      ADD COLUMN board_width FLOAT NULL;
     `);
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
-      ALTER TABLE machines
+      ALTER TABLE work_order
       DROP COLUMN unit,
       DROP COLUMN board_length,
       DROP COLUMN board_width;
