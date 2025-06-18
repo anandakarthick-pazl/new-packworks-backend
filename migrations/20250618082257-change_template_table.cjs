f38baefb79bc84f30bfb0686509d44da0980617b
SelvakumarMurugan <selvakumar.m@pazl.info>
2025-06-18 17:27:23 +0530

    template

--- migrations/20250618082257-change_template_table.cjs
+++ migrations/20250618082257-change_template_table.cjs
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
-      RENAME TABLE purchaseordertemplates TO pdf_templates;
+      RENAME TABLE purchaseordertemplate TO pdf_templates;
     `);
 
     await queryInterface.sequelize.query(`
