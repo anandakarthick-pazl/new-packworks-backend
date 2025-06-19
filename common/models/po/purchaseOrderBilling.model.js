f31aca2a6b1feaf9d17d794b75a022a676a5e90b
Columbus-12 <columbus.d@pazl.info>
2025-06-19 16:57:31 +0530

    bill table

---
+++ common/models/po/purchaseOrderBilling.model.js
@@ -0,0 +1,93 @@
+import { Sequelize, DataTypes } from "sequelize";
+import sequelize from "../../database/database.js";
+import Company from "../company.model.js";
+import User from "../user.model.js";
+import PurchaseOrder from "./purchase_order.model.js";
+
+const PurchaseOrderBilling = sequelize.define(
+  "PurchaseOrderBilling",
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    bill_generate_id: {
+      type: DataTypes.STRING(200),
+      allowNull: true,
+    },
+    company_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: true,
+      references: {
+        model: Company,
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+    },
+    purchase_order_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: PurchaseOrder, 
+        key: "id",
+      },
+    },
+    bill_reference_number: {
+      type: DataTypes.STRING(200),
+    },
+    bill_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    remarks: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM("active", "inactive"),
+      allowNull: false,
+      defaultValue: "active",
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    created_by: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: true,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+    updated_by: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: true,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+  },
+  {
+    tableName: "purchase_order_billings",
+    timestamps: false,
+  }
+);
+
+PurchaseOrderBilling.belongsTo(Company, { foreignKey: "company_id" });
+PurchaseOrderBilling.belongsTo(User, {
+  foreignKey: "created_by",
+  as: "creator",
+});
+PurchaseOrderBilling.belongsTo(User, {
+  foreignKey: "updated_by",
+  as: "updater",
+});
+
+export default PurchaseOrderBilling;
