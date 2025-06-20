b5382f42c1c4e5d2fd9cc81b010053b3209049a0
anandakarthick <anandakarthick.s1994@gmail.com>
2025-06-20 19:11:23 +0530

    code push
    code push

---
+++ migrations/20250620130535-add_menu-sale-return-menu.cjs
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // Step 1: Insert module
+    await queryInterface.sequelize.query(`
+      INSERT INTO modules (module_group, module_name, description, is_superadmin, order_by, created_at, updated_at, status, created_by, updated_by)
+      VALUES
+      ('Order Management', 'Sales Return', 'Sales Return', 0, 1, NOW(), NOW(), 'active', NULL, NULL)
+    `);
+
+    // Step 2: Fetch module ID
+    const [modules] = await queryInterface.sequelize.query(`
+      SELECT id FROM modules
+      WHERE module_name = 'Sales Return'
+      LIMIT 1
+    `);
+
+    const billsModuleId = modules[0]?.id;
+    if (!billsModuleId) throw new Error("Module 'Bills' not found");
+
+    // Step 3: Insert permission
+    await queryInterface.sequelize.query(`
+      INSERT INTO permissions (name, display_name, module_id, is_custom, allowed_permissions, created_at, updated_at, status, order_by)
+      VALUES 
+      ('add_sale_return', 'Add Sales Return', ${billsModuleId}, 1, '{"all":4, "none":5}', NOW(), NOW(), 'active', 1)
+    `);
+
+    // Step 4: Fetch permission ID
+    const [permissions] = await queryInterface.sequelize.query(`
+      SELECT id FROM permissions
+      WHERE name = 'add_sales_return'
+      LIMIT 1
+    `);
+
+    const addBillsPermissionId = permissions[0]?.id;
+    if (!addBillsPermissionId) throw new Error("Permission 'add_sale_return' not found");
+
+    // Step 5: Insert into permission_role
+    await queryInterface.sequelize.query(`
+      INSERT INTO permission_role (permission_id, role_id, permission_type_id, status)
+      VALUES 
+      (${addBillsPermissionId}, 4, 4, 'active')
+    `);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DELETE FROM permission_role
+      WHERE permission_id IN (
+        SELECT id FROM permissions WHERE name = 'add_sales_return'
+      )
+    `);
+
+    await queryInterface.sequelize.query(`
+      DELETE FROM permissions WHERE name = 'add_sales_return'
+    `);
+
+    await queryInterface.sequelize.query(`
+      DELETE FROM modules WHERE module_name = 'add_sales_return'
+    `);
+  }
+};
