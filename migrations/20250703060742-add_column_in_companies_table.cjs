0be80fe74439e59e8912631e80be817937707f83
Harish <harishashokumar07@gmail.com>
2025-07-03 15:26:35 +0530

    companies,model

--- migrations/20250703060742-add_column_in_companies_table.cjs
+++ migrations/20250703060742-add_column_in_companies_table.cjs
@@ -4,7 +4,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
 ALTER TABLE companies
-ADD COLUMN package_start_date DATE AFTER package_name,
+ADD COLUMN package_start_date DATE AFTER package_type,
 ADD COLUMN package_end_date DATE AFTER package_start_date;     
  `);
   },
